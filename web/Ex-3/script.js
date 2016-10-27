a558a3a01de502afe14bd47b0e19f6b40aa5a6b3
John <John@DESKTOP-34E2KUC>
2016-10-27 13:02:42 +0200

    Opg 3 complete

--- web/Ex-3/script.js
+++ web/Ex-3/script.js
@@ -1,16 +1,16 @@
 var app = angular.module("myApp", []);
 
-app.controller("MyController",['$scope',function($scope, RandomService){
+app.controller("MyController", function($scope, randomService){
         $scope.randomNumber = function (n) {
-            return Math.floor((Math.random() * n) + 1);
+            return randomService.getRandomNumber(n);
         };
         
         $scope.randomString = function (n) {
-          return new Array(n+1).join((Math.random().toString(36)+'00000000000000000').slice(2, 18)).slice(0, n)  
+          return randomService.getRandomString(n);
         };
-}]);
+});
 
-app.service("RandomService", function() {
+app.service("randomService", function() {
     this.getRandomNumber = function(n) {
         return Math.floor((Math.random() * n) + 1);
     };
