6412046d015406143509b8084491731b2ab53d8c
John <John@DESKTOP-34E2KUC>
2016-10-28 13:05:45 +0200

    Opg 3 complete

--- web/Ex-4/script.js
+++ web/Ex-4/script.js
@@ -1,25 +1,39 @@
 var app = angular.module("myApp", []);
 
-app.controller("MyController", function ($scope, countryFactory) {
-    countryFactory.allCountries(function(countryFactory) {
-       $scope.allCountries = countryFactory; 
-    });
-//    
-//    countryFactory.countriesByRegion("europe", function(countryFactory) {
-//       $scope.africa = countryFactory; 
-//    });
+app.controller("MyController", ['$scope', 'countryFactory', function ($scope, countryFactory) {
+        $scope.allCountries = [];
+        countryFactory.allCountries().success(function (data) {
+            $scope.allCountries = data;
+        });
 
+        $scope.africa = [];
+        countryFactory.countriesByRegion("africa").success(function (data) {
+            $scope.africa = data;
+        });
 
-});
+        $scope.europe = [];
+        countryFactory.countriesByRegion("europe").success(function (data) {
+            $scope.europe = data;
+        });
 
-app.factory("countryFactory", function ($http) {
-    return {
-      allCountries: function(callback) {
-          $http.get("https://restcountries.eu/rest/v1/all").success(callback);
-      },
-      
-      countriesByRegion: function(region) {
-          return $http.get("https://restcountries.eu/rest/v1/region/" + region).success();
-      }
-    };
-});
+        $scope.asia = [];
+        countryFactory.countriesByRegion("asia").success(function (data) {
+            $scope.asia = data;
+        });
+    }]);
+
+app.factory("countryFactory", ['$http', function ($http) {
+        return {
+            allCountries: function () {
+                return $http({
+                    url: 'https://restcountries.eu/rest/v1/all',
+                    method: 'get'
+                });
+            },
+            countriesByRegion: function (region) {
+                return $http({
+                    url: 'https://restcountries.eu/rest/v1/region/' + region,
+                    method: 'get',
+                });
+            }};
+    }]);
